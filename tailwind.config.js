c916888632e470cc84a424ffa21ad262cbb4abd6
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-07-28 15:25:24 -0700

    build: background for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
+  darkMode: 'class', // or 'media' or 'class ,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '',
+        darkBlue: 'hsl(217, 28%, 15%)',
+      },
+    },
   },
   plugins: [],
 }
