c47ff932349027e119ef6bf333ee6991b23d2b42
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-07-28 18:03:25 -0700

    build: hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './shared/modules/header/header.module';
+import { HeroModule } from './shared/modules/hero/hero.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HeaderModule } from './shared/modules/header/header.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeaderModule
+    HeaderModule,
+    HeroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
